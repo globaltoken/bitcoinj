c4472e8e13765cc7286cdc1ca148c08292fd34b6
globaltoken <heisenbergakatheking@gmail.com>
2017-08-22 13:16:25 +0200

    Buildcheckpoints wrong Checkstate
    Buildcheckpoints wrong Checkstate

--- tools/src/main/java/org/bitcoinj/tools/BuildCheckpoints.java
+++ tools/src/main/java/org/bitcoinj/tools/BuildCheckpoints.java
@@ -222,15 +222,15 @@ public class BuildCheckpoints {
         checkState(manager.numCheckpoints() == expectedSize);
 
         if (params.getId().equals(NetworkParameters.ID_MAINNET)) {
-            StoredBlock test = manager.getCheckpointBefore(1390500000); // Thu Jan 23 19:00:00 CET 2014
-            checkState(test.getHeight() == 280224);
+            StoredBlock test = manager.getCheckpointBefore(1502699458); // Thu Jan 23 19:00:00 CET 2014
+            checkState(test.getHeight() == 222158);
             checkState(test.getHeader().getHashAsString()
-                    .equals("00000000000000000b5d59a15f831e1c45cb688a4db6b0a60054d49a9997fa34"));
+                    .equals("000000000000036b0385aea5c398010f73a609ec9920673bcd4c765151a0e060"));
         } else if (params.getId().equals(NetworkParameters.ID_TESTNET)) {
-            StoredBlock test = manager.getCheckpointBefore(1390500000); // Thu Jan 23 19:00:00 CET 2014
-            checkState(test.getHeight() == 167328);
+            StoredBlock test = manager.getCheckpointBefore(1483418896);
+            checkState(test.getHeight() == 1);
             checkState(test.getHeader().getHashAsString()
-                    .equals("0000000000035ae7d5025c2538067fe7adb1cf5d5d9c31b024137d9090ed13a9"));
+                    .equals("0000000097fcef2abcd5b827feffb2625fa50ef1794d8efc75ddbfaa3daeb499"));
         }
     }
 
